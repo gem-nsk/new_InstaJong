2dfe418ddd5333e5514add6babacd0391d086236
Bjorn Sodenberg <garonini1@gmail.com>
2019-09-11 14:59:03 +0700

    Добавил вывод фото в нормальном разрешении, имя пользователя и описание к публикации

--- Assets/Scripts/ImagePreviewer.cs
+++ Assets/Scripts/ImagePreviewer.cs
@@ -7,16 +7,24 @@ public class ImagePreviewer : MonoBehaviour
 {
     public Image img;
     public Animator anim;
+    public Text description;
+    public Text username;
 
     public void Preview(int id)
     {
         LoadPicture(id);
+        setDescriprion(id);
+        setUsername(id);
     }
 
     void LoadPicture(int id)
     {
-        Material mat = AtlasController.instance.GetMaterialById(id);
-        img.material = mat;
+        //Material mat = AtlasController.instance.GetMaterialById(id);
+
+        //img.material = mat;
+        //Material mat = new Material(fileStandard);
+        //Debug.Log(id);
+        img.sprite = Resources.Load<Sprite>("imageStandard/file"+id);
     }
     public void Close()
     {
@@ -28,5 +36,16 @@ public class ImagePreviewer : MonoBehaviour
         anim.SetTrigger("Close");
         yield return new WaitForSeconds(0.3f);
         gameObject.SetActive(false);
+    }
+
+    public void setDescriprion(int id)
+    {
+        Debug.Log(id);
+        description.text = AtlasController.instance.posts[id-1].description;
+    }
+
+    public void setUsername(int id)
+    {
+        username.text = AtlasController.instance.posts[id - 1].usernameFrom;
     }
 }
